c0b876c5e88ce717071c4ae40c73f734ca9b629b
Marc Laliberte <marclaliberte@gmail.com>
2020-12-16 13:06:34 -0800

    Fixed merge artifact bug

--- src/services/app.service.js
+++ src/services/app.service.js
@@ -387,19 +387,11 @@ function setAppConfig() {
       gameDir: {
         win: [
           'World of Warcraft/_classic_beta_/WowClassicB.exe',
-<<<<<<< HEAD
         ],
         mac: ['World of Warcraft/_classic_beta_/World of Warcraft Classic Beta.app'],
         linux: [
           'World of Warcraft/_classic_beta_/WowClassicB.exe',
         ],
-=======
-        ],
-        mac: ['World of Warcraft/_classic_beta_/World of Warcraft Classic Beta.app'],
-        linux: [
-          'World of Warcraft/_classic_beta_/WowClassicB.exe',
-        ],
->>>>>>> develop
       },
     };
     setGameData('1', wowD);
@@ -431,7 +423,6 @@ function setAppConfig() {
       unknownAddonDirs: [],
     };
     setGameSettings('1', wowS);
-<<<<<<< HEAD
     const userConfig = getAppData('userConfigurable');
     userConfig.telemetry = false;
     setAppData('userConfigurable', userConfig);
@@ -447,8 +438,6 @@ function setAppConfig() {
     setAppData('privacy', privSettings);
     setAppData('tos', tosSettings);
     setAppData('terms', terms);
-=======
->>>>>>> develop
   }
 
   // Set new version
