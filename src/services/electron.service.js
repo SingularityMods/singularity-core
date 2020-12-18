7abab1436a372dfffeacb768baf17e5dda47b4e6
Marc Laliberte <marclaliberte@gmail.com>
2020-12-18 12:14:47 -0800

    Remove alpha testing location

--- src/services/electron.service.js
+++ src/services/electron.service.js
@@ -106,8 +106,8 @@ function downloadLatestAppVersion(window, version) {
         updateFilePath = `${AppConfig.PACKAGE_URL}/Win/Beta/${fileName}`;
         updateFeedPath = `${AppConfig.PACKAGE_URL}/Win/Beta/${manifestName}`;
       } else {
-        updateFilePath = `${AppConfig.PACKAGE_URL}/Win/Alpha/${fileName}`;
-        updateFeedPath = `${AppConfig.PACKAGE_URL}/Win/Alpha/${manifestName}`;
+        updateFilePath = `${AppConfig.PACKAGE_URL}/Win/${fileName}`;
+        updateFeedPath = `${AppConfig.PACKAGE_URL}/Win/${manifestName}`;
       }
       feedUrl = { url: tempPath };
     } else if (process.platform === 'darwin') {
