a4a7eea528e511c8dfead608c679554cab72bdb2
Marc Laliberte <marclaliberte@gmail.com>
2021-01-18 20:10:53 -1000

    Additional work

--- src/services/singularity.service.js
+++ src/services/singularity.service.js
@@ -4,7 +4,16 @@ import queryString from 'query-string';
 
 // import { getAccessToken, isAuthenticated } from '../../services/auth.service';
 import AppConfig from '../config/app.config';
-import { getAppData, getAddonVersion } from './storage.service';
+import { 
+  getAddonDir,
+  getAppData,
+  getAddonVersion,
+  updateInstalledAddonInfo
+} from './storage.service';
+import {
+  installAddon,
+  handleInstallDependencies,
+} from './file.service';
 import { getMainBrowserWindow } from './electron.service';
 
 function getAddonsFromFingerprints(directories) {
@@ -103,36 +112,91 @@ function searchForAddons(
   });
 }
 
+function installAddonFromProtocolUrl(addon, fileId) {
+  return new Promise((resolve, reject) => {
+    getAddonDownloadUrl(addon.addonId, fileId)
+      .then((fileInfo) => {
+        let gameId = 1;
+        let gameVersion = fileInfo.fileDetails.gameVersionFlavor;
+        if (gameVersion === 'wow_retail'
+          || gameVersion === 'wow_classic') {
+            gameId = 1;
+          } else if (gameVersion === 'eso') {
+            gameId = 2;
+          }
+        const addonDir = getAddonDir(gameId, gameVersion);
+        installAddon(addonDir, fileInfo.downloadUrl)
+        .then(() => {
+          updateInstalledAddonInfo(gameId, gameVersion, addon, fileInfo.fileDetails)
+        })
+        .then((installedAddon) => handleInstallDependencies(gameId, gameVersion, installedAddon))
+        .then((installedAddon) => {
+          const gameS = getGameSettings(gameId.toString());
+          if (gameS[gameVersion].sync && isAuthenticated()) {
+            log.info('Game version is configured to sync, updating profile');
+            return createAndSaveSyncProfile({ gameId, gameVersion })
+              .then(() => {
+                log.info('Sync profile updated');
+                return resolve(installedAddon);
+              })
+              .catch((err) => {
+                log.error('Error saving sync profile');
+                log.error(err);
+                return resolve(installedAddon);
+              });
+          }
+          log.info(`Succesfully installed ${installedAddon.addonName}`);
+          return resolve(installedAddon);
+        })
+      })
+      .catch((error) => {
+        log.error(error.message);
+        return reject(error);
+      });
+  })
+}
+
 function handleProtocolUrl(url) {
-  console.log(url);
-  const urlParts = url.split('?');
-  if (!urlParts || urlParts.length < 2) {
-    return null;
-  }
-  const query = queryString.parse(urlParts[1]);
-  let addonId;
-  let fileId;
-  let clusterId;
-  if ('addonId' in query) {
-    addonId = query.addonId;
-  }
-  if ('fileId' in query) {
-    fileId = query.fileId;
-  }
-  if ('clusterId' in query) {
-    clusterId = query.clusterId;
-  }
-  if (!addonId && !clusterId) {
-    return null;
-  }
-  const win = getMainBrowserWindow();
-  if (!win) {
-    return null;
-  }
-  if (addonId) {
-    return win.webContents.send('install-addon-from-protocol', addonId, fileId);
-  }
-  return win.webContents.send('install-cluster-from-protocol', clusterId);
+  return new Promise((resolve, reject) => {
+    console.log(url);
+    const urlParts = url.split('?');
+    if (!urlParts || urlParts.length < 2) {
+      return reject(new Error('Invalid download URL'));
+    }
+    const win = getMainBrowserWindow();
+    const query = queryString.parse(urlParts[1]);
+    let addonId;
+    let fileId;
+    let clusterId;
+    if ('addonId' in query) {
+      addonId = query.addonId;
+    }
+    if ('fileId' in query) {
+      fileId = query.fileId;
+    }
+    if ('clusterId' in query) {
+      clusterId = query.clusterId;
+    }
+    if (!addonId && !clusterId) {
+      return reject( new Error('Missing addonId'));
+    }
+    getAddonInfo(addonId)
+      .then((addon) => {
+        installAddonFromProtocolUrl(addon,fileId)
+    })
+    /*
+    const win = getMainBrowserWindow();
+    if (!win) {
+      return reject( new Error('Singularity app window is closed, cannot install addon from link'));
+    }
+    if (addonId) {
+      return getAddonDownloadUrl(addonId, fileId)
+      return win.webContents.send('install-addon-from-protocol', addonId, fileId);
+    }
+    return win.webContents.send('install-cluster-from-protocol', clusterId);
+    */
+  })
+
 }
 
 export {
