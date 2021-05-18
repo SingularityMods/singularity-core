6d242bea135f3a86b179c42b42b2ff678d77e2fc
mlalibs <marclaliberte@gmail.com>
2021-05-18 12:03:13 -0700

    Remove trailing slash to prevent doubleslash in URI path

--- src/config/app.config.prod.js
+++ src/config/app.config.prod.js
@@ -1,7 +1,7 @@
 const prodConfig = {
   API_URL: 'https://api.singularitymods.com/api/v2',
   AUTH_API_URL: 'https://id.singularitymods.com',
-  PACKAGE_URL: 'https://storage.singularitycdn.com/App/Releases/',
+  PACKAGE_URL: 'https://storage.singularitycdn.com/App/Releases',
   SENTRY_DSN: 'https://737029cdbe184e2c9c689773e58bf9f4@o482473.ingest.sentry.io/5532772',
   SENTRY_ENV: 'Production',
 };
